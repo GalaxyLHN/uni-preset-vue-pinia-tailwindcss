f83e10eede79d93900650a7ecdff5811eee2b859
GalaxyLHN <GalaxyLHN@outlook.com>
2026-02-13 04:28:38 +0800

    完成js配置

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,20 @@
 import { defineConfig } from 'vite'
 import uni from '@dcloudio/vite-plugin-uni'
+import { UnifiedViteWeappTailwindcssPlugin as uvwt } from "weapp-tailwindcss/vite";
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     uni(),
+    uvwt({
+      rem2rpx: true,
+    }),
   ],
+  css: {
+    postcss: {
+      plugins: [tailwindcss(), autoprefixer()],
+    },
+  },
 })
