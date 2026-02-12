6f5270454d1ee174b23c82cdee737637b781c466
GalaxyLHN <43498820+GalaxyLHN@users.noreply.github.com>
2026-02-13 04:40:22 +0800

    删除了不必要的注释

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,11 @@ import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 export function createApp() {
   const app = createSSRApp(App);
   const pinia = Pinia.createPinia();
-  pinia.use(piniaPluginPersistedstate); // 使用持久化插件，确保用户登录信息等数据不会因为关掉小程序而丢失
+  pinia.use(piniaPluginPersistedstate);
   app.use(pinia);
   return {
     app,
     Pinia,
   };
 }
+
